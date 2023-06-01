e9006e4e622f669367cd55e994ec8a34e801e192
Jesusescudero <116468282+Jesusescudero@users.noreply.github.com>
2023-05-31 21:17:49 -0600

    Add files via upload

--- P2/adi.js
+++ P2/adi.js
@@ -8,16 +8,16 @@ function adivinarNumero() {
 
   if (numero == numAleatorio) {
     mensaje = "¡Felicidades! Adivinaste el número en " + intentos + " intentos";
-    document.getElementById("resultado").style.color = "green";
+    document.getElementById("resultado");
     document.getElementById("numero").disabled = true;
     alert(mensaje);
   } else if (numero < numAleatorio) {
     mensaje = "El número ingresado es menor al número buscado";
-    document.getElementById("resultado").style.color = "red";
+    document.getElementById("resultado");
     alert(mensaje);
   } else {
     mensaje = "El número ingresado es mayor al número buscado";
-    document.getElementById("resultado").style.color = "red";
+    document.getElementById("resultado");
     alert(mensaje);
   }
 
